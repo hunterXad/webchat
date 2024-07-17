f6f201d091a713a2b2acf92476866b87fa96e9cd
Baker ahmed <109065379+hunterXad@users.noreply.github.com>
2024-07-18 02:12:55 +0300

    Update settings.py

--- chatpro/settings.py
+++ chatpro/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4f@w+1b%ib=f^4nl3&ib@6ccu#v5@jtmp^*03%^qmj0k76!a&u'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
